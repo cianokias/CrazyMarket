4cc84eded885c1cbc3dda19e703d59cb77c9fe99
CsOHQAQ <529353999@qq.com>
2024-08-22 22:32:06 -0600

    *完成了Astar算法（注意：未经过测试版）

--- Assets/Scripts/NPC_SceneControl.cs
+++ Assets/Scripts/NPC_SceneControl.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class NPC_SceneControl : MonoBehaviour
 {
-    GameObject player;
+    public static NPC_SceneControl NPC_Scene;
+    public GameObject player;
     public GameObject box;
     public Transform obj_trans;
     public int box_Num = 5;
@@ -13,17 +14,23 @@ public class NPC_SceneControl : MonoBehaviour
     public int NPC_Num = 3;
     public float NPC_Speed=2;
 
+    public int[,] MapInfo;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        
-
+        MapInfo=new int[17,17];
+        NPC_Scene = this;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateBox();
+        }
+        RefreshMapInfo();
     }
 
 
@@ -33,14 +40,38 @@ public class NPC_SceneControl : MonoBehaviour
         int ori_num = obj_trans.childCount;
         for (int i = 0; i < ori_num; i++)
         {
-            Destroy(obj_trans.GetChild(0));
+            Destroy(obj_trans.GetChild(i).gameObject);
         }
 
+        //Generate New box
         for (int i = 0; i < box_Num; i++) 
         {
             GameObject new_box= GameObject.Instantiate(box,obj_trans);
+            new_box.transform.position = new Vector2((int)(Random.value*13+1.5),(int)(Random.value*13+1.5));
 
         }
     }
+    public void RefreshMapInfo()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j <16; j++)
+            {
+                if (i == 0 || i == 15 || j == 0 || j == 15)
+                {
+                    MapInfo[i, j] = 19260817;
+                }
+            }
+        }
 
+        for (int i = 0;i < obj_trans.childCount; i++)
+        {
+            var item= obj_trans.GetChild(i);
+            int x=(int)item.position.x,y=(int)item.position.y;
+            if (item.tag == "Boundary"|| item.tag == "Box")
+            {
+                MapInfo[x, y] = 114;
+            }
+        }
+    }
 }
